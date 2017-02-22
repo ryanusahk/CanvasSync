af469d08b5a4afe7b356ccfd857a096e64ec8ebe
ryanusahk <Hon@Hons-MacBook-Pro.local>
2017-02-21 22:37:45 -0800

    so many features
    1. defaults to bcourses.berkeley.edu
    2. added guided setup
    2a. choose which classes to sync
    2b. choose what folder to sync each class to
    3. fixed max 10 class and max 10 file downloads per run.

--- CanvasSync.py
+++ CanvasSync.py
@@ -5,7 +5,6 @@ import os, sys, time
 from urllib2 import HTTPError
 import os.path
 
-#TODO: Check the list of files JSON for recent files
 home = os.path.expanduser("~")
 
 website = 'https://bcourses.berkeley.edu'
@@ -16,17 +15,25 @@ readout_mode = len(sys.argv) > 1
 course_to_folder = {}
 
 
+
 if not os.path.isfile(settings_file):
     print 'Welcome to the setup!'
     course_to_folder['ACCESS_TOKEN__'] = raw_input('Paste your access token: ')
-    courses_url = website + courses_api + '?access_token=' + course_to_folder['ACCESS_TOKEN__']
+    courses_url = website + courses_api + '?per_page=100&access_token=' + course_to_folder['ACCESS_TOKEN__']
     response = urllib2.urlopen(courses_url).read()
     courses = json.loads(response) 
 
 
     print 'Where do you want to sync to? Make sure the directory already exists.'
-    course_to_folder['directory__'] = raw_input('Enter the directory relative to ~ (start with /): ')
+    response = raw_input('Enter the directory relative to ~ (start with /): ')
 
+    if response[0] != '/':
+        response = '/' + response
+    if response[-1] != '/':
+        response = response + '/'
+    course_to_folder['directory__'] = response
+
+    course_to_folder['directory__']
     print 'Enter a folder name for each class, or a . to not sync the class'
     for course in courses:
         response = raw_input(course['name'] + '\t')
@@ -41,62 +48,83 @@ course_to_folder = json.loads(open(settings_file, 'r').read())
 access_token = 'access_token=' + course_to_folder['ACCESS_TOKEN__']
 parent_dir = home + course_to_folder['directory__']
 
+
+
+# testurl = website + '/api/v1/users/self/files?' + access_token
+# response = urllib2.urlopen(testurl).read()
+# testout = json.loads(response) 
+
+# print testout
+
+
+
+
+
 # Fetch the course details
-courses_url = website + courses_api + '?' + access_token
+courses_url = website + courses_api + '?per_page=100&' + access_token
 response = urllib2.urlopen(courses_url).read()
 courses = json.loads(response)              # Parse the JSON response
 
 
 for course in courses:
     # Create course folders
-
-    if course['name'] not in course_to_folder:
+    if 'name' not in course or course['name'] not in course_to_folder:
         continue
-
     course_name = course_to_folder[course['name']]    # Splitting the name to make it simple
-    #TODO: generic course name
+    print 'Syncing ' + course_name
     course_dir = parent_dir + course_name
     if not os.path.exists(course_dir):
         print 'Creating ', course_dir
         os.makedirs(course_dir)
     course_id = course['id']
 
+    # print course_id
+
     # Create sub-folders
-    folder_url = website + courses_api + '/' + str(course_id) +'/folders?'+ access_token
+    folder_url = website + courses_api + '/' + str(course_id) +'/folders?per_page=100&'+ access_token
     try:
         folder_response = urllib2.urlopen(folder_url).read()
         folders = json.loads(folder_response)
-
+        # print folders, '\n\n'
         folder_dict = {}
         # Create Sub-folders
         for folder in folders:
             # Creating a dict to store folder name as file object has only folder ID
-            folder_name = folder['name']
+            folder_name = folder['full_name']
             folder_dict[folder['id']] = folder_name
             folder_dir = course_dir + '/' + folder_name
+            folder_dir = folder_dir.replace('/course files', '')
             if not os.path.exists(folder_dir):
                 print 'Creating', folder_dir
                 os.makedirs(folder_dir)
 
         # Downloading files in the respective folders
         try:
-            files_url = website + courses_api + '/' + str(course_id) + '/files?' + access_token
-            files_response = urllib2.urlopen(files_url).read()
-            # Parsing the JSON response
-            files = json.loads(files_response)
-            for file in files:
-                # Deciding file location
-                file_location = course_dir + '/' + folder_dict[file['folder_id']] + '/'
-                file_url = file['url']
-                file_name = file['filename']
-                file_path = file_location + urllib.unquote(file_name).replace('+', ' ')
-                #TODO: time.ctime(os.path.getmtime(file)) != file['updated_at']
-                if not os.path.isfile(file_path):
-                    print 'Downloading ', file_path.split('/')[-1]
-                    urllib.urlretrieve(file_url, file_path)
-                elif readout_mode:
-                    print 'Skipping    ', file_path.split('/')[-1]                    
+            p_num = 1
+            while True:
+                files_url = website + courses_api + '/' + str(course_id) + '/files?page=' + str(p_num) + '&per_page=20&' + access_token
+                files_response = urllib2.urlopen(files_url).read()
+                if files_response == '[]':
+                    break
+                # Parsing the JSON response
+                files = json.loads(files_response)
+                for file in files:
+                    # print file['filename']
+                    # print file
+                    # Deciding file location
+                    file_location = course_dir + '/' + folder_dict[file['folder_id']] + '/'
+                    file_url = file['url']
+                    file_name = file['display_name']
+                    file_path = file_location + urllib.unquote(file_name).replace('+', ' ')
+                    file_path = file_path.replace('/course files', '')
 
+                    #TODO: time.ctime(os.path.getmtime(file)) != file['updated_at']
+                    if not os.path.isfile(file_path):
+                        print 'Downloading ', file_path.split('/')[-1]
+                        urllib.urlretrieve(file_url, file_path)
+                    elif readout_mode:
+                        print 'Skipping    ', file_path.split('/')[-1]                    
+                p_num += 1
         except HTTPError as fileE:
             print 'No files for course', course_name
     except HTTPError as folderE:
